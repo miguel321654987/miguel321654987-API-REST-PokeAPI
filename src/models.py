6fc218abcceb3b4b8681e2dec65791d53665fa66
miguel321654987 <lacarricanta@hotmail.com>
2026-07-13 14:01:44 +0200

    Añadir código guardado de Students y Staff

--- src/models.py
+++ src/models.py
@@ -28,3 +28,30 @@ class People(db.Model):
             "id": self.id,
             "people_name": self.people_name,
         }
+
+
+class Students(Model):
+    id: Mapped[int]
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class Staff(Model):
+    id: Mapped[int]
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
