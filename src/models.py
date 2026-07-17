4177e8e76f598ccf2c1d012f6c18894bc12b7e33
miguel321654987 <lacarricanta@hotmail.com>
2026-07-17 12:29:03 +0000

    cambios en models

--- src/models.py
+++ src/models.py
@@ -19,43 +19,15 @@ class User(db.Model):
         }
 
 
-class People(db.Model):
-    __tablename__ = 'people'
+class Pokemon(db.Model):
+    __tablename__ = 'pokemon'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    people_name: Mapped[str] = mapped_column(
+    pikmin_name: Mapped[str] = mapped_column(
         String(50), unique=True, nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "people_name": self.people_name,
-        }
+def serialize(self):
+    return {
+        "id": self.id,
+        "pikmin_name": self.pikmin_name,
+    }
 
-
-class Students(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
-
-
-class Staff(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-        }
