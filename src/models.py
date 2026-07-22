24d1ec33458b18f95b7eb2612e4181a06db3c213
miguel321654987 <lacarricanta@hotmail.com>
2026-07-22 10:59:52 +0000

    reconfig a contextLength: 16384

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer
+# <-- Agregando 'select' aquí
+from sqlalchemy import String, Boolean, Integer, select
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
