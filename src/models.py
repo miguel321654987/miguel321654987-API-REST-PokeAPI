ef0ffe10f40fedbfd48948e04672f9f86db8d25e
miguel321654987 <lacarricanta@hotmail.com>
2026-07-24 12:25:02 +0000

    crear pokemon_bp

--- src/models.py
+++ src/models.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-# <-- Agregando 'select' aquí
 from sqlalchemy import String, Boolean, Integer, select
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
-db = SQLAlchemy()
+# 1. Creamos la clase base necesaria para que funcione el 'init=True'
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+# 2. Le pasamos esa base a SQLAlchemy
+db = SQLAlchemy(model_class=Base)
 
 
 class User(db.Model):
@@ -11,9 +18,11 @@ class User(db.Model):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+        String(120), unique=True, nullable=False, init=True)
+    password: Mapped[str] = mapped_column(
+        String(250), nullable=False, init=True)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(), nullable=False, init=True)
 
     def serialize(self):
         """Serializa el objeto User"""
@@ -30,7 +39,8 @@ class Pokemon(db.Model):
     __tablename__ = 'pokemon'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    people_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    people_name: Mapped[str] = mapped_column(
+        String(50), nullable=False, init=True)
 
     def serialize(self):
         """Serializa el objeto Pokemon"""
