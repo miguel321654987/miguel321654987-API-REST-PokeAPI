b509c31794cb3aabce2dc32976d6c8170c199344
miguel321654987 <lacarricanta@hotmail.com>
2026-07-30 15:18:40 +0000

    añadir favorites_bp

--- src/Backend/routes.py
+++ src/Backend/routes.py
@@ -2,15 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Blueprint
-from flask_cors import CORS
 from Backend.blueprints.user_bp import user_bp
 from Backend.blueprints.pokemon_bp import pokemon_bp
+from Backend.blueprints.favorites_bp import favorites_bp
 
 api = Blueprint('api', __name__)
 
 # REGISTRO DE BLUEPRINTS
-# Todas las rutas de usuarios comenzarán con /auth (ej: /auth/login)
+# REGISTRO DE BLUEPRINTS
 api.register_blueprint(user_bp, url_prefix='/auth')
-
-# Todas las rutas de pokémon comenzarán con /pok (ej: /pok/pokemon)
 api.register_blueprint(pokemon_bp, url_prefix='/pok')
+api.register_blueprint(favorites_bp, url_prefix='/favorites')
